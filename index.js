d0a889fab372fe11b583ddf60973066e0b006413
Vassilis Zafeiris <bzafiris@gmail.com>
2021-05-15 21:44:59 +0300

    Added basic files

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8080
+
+app.listen(port)
+
+/* 
+    Serve static content from directory "public",
+    it will be accessible under path /static, 
+    e.g. http://localhost:8080/static/index.html
+*/
+app.use('/static', express.static(__dirname + '/public'))
+
+// parse url-encoded content from body
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json content from body
+app.use(express.json())
+
+// serve index.html as content root
+app.get('/', function(req, res){
+
+    var options = {
+        root: path.join(__dirname, 'public')
+    }
+
+    res.sendFile('index.html', options, function(err){
+        console.log(err)
+    })
+})
